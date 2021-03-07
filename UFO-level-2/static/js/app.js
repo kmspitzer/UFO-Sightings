2d5cc484292223b6417c41a8793468268bde10b0
kate <kate_spitzer@yahoo.com>
2021-03-07 15:31:12 -0800

    added new input fields

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -21,9 +21,8 @@ var button = d3.select("#filter-btn");
 var form = d3.select("#form");
 
 
-// Create event handlers for clicking the button or pressing the enter key
+// Create event handlers for clicking the button
 button.on("click", runEnter);
-form.on("submit",runEnter);
 
 // Create the function to run for both events
 function runEnter() {
@@ -32,10 +31,35 @@ function runEnter() {
   d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  var inputDate = d3.select("#datetime");
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var dateValue = inputDate.property("value");
+
+  // Select the input element and get the raw HTML node
+  var inputCity = d3.select("#city");
+
+  // Get the value property of the input element
+  var cityValue = inputCity.property("value");
+
+  // Select the input element and get the raw HTML node
+  var inputState = d3.select("#state");
+
+  // Get the value property of the input element
+  var stateValue = inputState.property("value");
+
+  // Select the input element and get the raw HTML node
+  var inputCountry = d3.select("#country");
+
+  // Get the value property of the input element
+  var countryValue = inputCountry.property("value");
+
+    // Select the input element and get the raw HTML node
+    var inputShape = d3.select("#shape");
+
+    // Get the value property of the input element
+    var shapeValue = inputShape.property("value");
+
 
   if (inputValue === "") {
     var filteredData = tableData;
